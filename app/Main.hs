133015da489c7e237b2819c3888f5825fa9f4b3d
107zxz <i107zxz@icloud.com>
2024-09-27 20:05:30 +0930

    Ideally got the bot going

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+{-# LANGUAGE OverloadedStrings #-}
 module Main (main) where
 
 import           Control.Monad
 -- import           UnliftIO.Concurrent
 import qualified Data.Text.IO as TIO
-import qualified Data.Text as T
 
 import           Discord
 import qualified Discord.Requests as R
@@ -12,38 +11,47 @@ import qualified Discord.Requests as R
 import Debug.Trace
 import Data.Text (pack)
 
+import System.Random.Shuffle
+import System.Random
+import Text.Printf (printf)
+
+
 main :: IO ()
 main = do
 
-    -- allPrompts <- readPrompts
-    -- let idxs = randomRs (0 length allPrompts) getStdGen :: [Int]
-    -- bigThree <- take 3 $ [allPrompts !! x | x <- randomRs (0, length allPrompts) getStdGen]
+    gen <- newStdGen
+    allPrompts <- readPrompts
+    let chosen = samplePrompts allPrompts gen
+    let numPrompts = 3
 
     secret <- readFile "secret.txt"
 
     userFacingError <- runDiscord $ def
              { discordToken = pack secret
              , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
-             , discordOnStart = do sendPrompts; stopDiscord
+             , discordOnStart = do sendPrompts $ chosen numPrompts; stopDiscord
              } -- if you see OnLog error, post in the discord / open an issue
 
     TIO.putStrLn userFacingError
     -- userFacingError is an unrecoverable error
     -- put normal 'cleanup' code in discordOnEnd (see examples)
 
--- readPrompts :: IO [String]
--- readPrompts =
-    -- lines <$> readFile "prompts.txt"
+readPrompts :: IO [String]
+readPrompts =
+    lines <$> readFile "prompts.txt"
 
--- samplePrompts :: [String] -> [String]
--- samplePrompts prompts =
---     random
+samplePrompts :: [String] -> StdGen -> Int -> [String]
+samplePrompts prompts gen count = do
+    take count $ shuffle' prompts (length prompts) gen
 
-sendPrompts :: DiscordHandler ()
-sendPrompts =
-    sendMessage "Fuckk yeah!"
+sendPrompts :: [String] -> DiscordHandler ()
+sendPrompts prompts = do
+    let promptstring = unwords ["\n**" ++ p ++ "**" | p <- prompts]
+    let message = printf "Today's weekly prompts are: " ++ promptstring
 
-sendMessage :: T.Text -> DiscordHandler ()
+    sendMessage message
+
+sendMessage :: String -> DiscordHandler ()
 sendMessage message =
-    trace ("Sending message: " ++ show message)
-    void $ restCall $ R.CreateMessage (read "788239141548589060") message
+    trace ("Sending message: " ++ message)
+    void $ restCall $ R.CreateMessage (read "788239141548589060") (pack message)
